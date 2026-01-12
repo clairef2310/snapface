341b9b6f543ca382a8c90165665ae9ac405c8043
ClaireF <claire.fernandes.ee@gmail.com>
2026-01-12 12:48:58 +0100

    add data-binding and reactivity

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
   imports: [],
   templateUrl: './face-snap.component.html',
-  styleUrl: './face-snap.component.scss'
+  styleUrl: './face-snap.component.scss',
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+  snapBtnText!: string;
+  userHasLike!: boolean;
 
+  ngOnInit(): void {
+    this.snapBtnText = "J'aime";
+    this.userHasLike = false;
+  }
+
+  onSnap(): void {
+    if (this.userHasLike) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.faceSnap.removeSnaps();
+    this.snapBtnText = "J'aime";
+    this.userHasLike = false;
+  }
+
+  snap() {
+    this.faceSnap.addSnaps();
+    this.snapBtnText = "Enlever le j'aime";
+    this.userHasLike = true;
+  }
 }
